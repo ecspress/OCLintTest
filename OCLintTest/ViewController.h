27509061a9c04f1896e2333cdb35912c8b0b5893
Faisal Saleh <faisal.saleh@tenpearls.com>
2017-03-27 14:22:17 +0500

    Initial Commit

---
+++ OCLintTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCLintTest
+//
+//  Created by Faisal Saleh on 27/03/2017.
+//  Copyright © 2017 TenPearls. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCLintTest/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  OCLintTest
+//
+//  Created by Faisal Saleh on 27/03/2017.
+//  Copyright © 2017 TenPearls. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
